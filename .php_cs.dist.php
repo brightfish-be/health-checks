779ed97d2371046ae2452847dd5729b0ec42fcfc
dotburo <code@dotburo.org>
2023-04-07 14:54:04 +0300

    Updated gh workflows

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -1,16 +1,15 @@
 <?php
 
-$finder = Symfony\Component\Finder\Finder::create()
+$finder = \PhpCsFixer\Finder::create()
     ->in([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
     ->name('*.php')
-    ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return (new PhpCsFixer\Config())
+return \PhpCsFixer\Config::create()
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
